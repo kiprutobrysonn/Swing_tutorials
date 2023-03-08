e6da96806f4785299fa2bec49b11b0999898c7d7
brycekip46 <brycekip46@gmail.com>
2023-03-08 15:37:38 +0300

    Added text area

--- src/swing_tutorial/Panels.java
+++ src/swing_tutorial/Panels.java
@@ -3,58 +3,65 @@ package swing_tutorial;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import javax.swing.border.BevelBorder;
 
 public class Panels{// panels that have borders an aligned
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JPanel topPanel = new JPanel();
+		JTextArea text = new JTextArea(800,800);
+		
+		text.setAutoscrolls(true);
 		
 		JButton fileButton = new JButton();
-		topPanel.setLayout(null);
+		// use of flow layout to place buttons in a application
+		topPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
 		fileButton.setText("Files");
-		fileButton.setBounds(0,0,100,20);
-		fileButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		fileButton.setBorder(BorderFactory.createEmptyBorder());
 		fileButton.setBackground(Color.white);
 		
 		JButton viewButton = new JButton();
 		viewButton.setText("Views");
-		viewButton.setBounds(100,0,100,20);
-		viewButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		viewButton.setBorder(BorderFactory.createEmptyBorder());
 		viewButton.setBackground(Color.white);
 		
 		
 		JButton toolsButton = new JButton();
 		toolsButton.setText("Tools");
-		toolsButton.setBounds(200,0,100,20);
-		toolsButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		toolsButton.setBorder(BorderFactory.createEmptyBorder());
 		toolsButton.setBackground(Color.white);
 		
 		JButton helpButton = new JButton();
 		helpButton.setText("Help");
-		helpButton.setBounds(300,0,100,20);
-		helpButton.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		helpButton.setBorder(BorderFactory.createEmptyBorder());
 		helpButton.setBackground(Color.white);
 	
-		topPanel.setPreferredSize(new Dimension(100,50));
+		topPanel.setPreferredSize(new Dimension(100,30));
+		topPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
 		topPanel.add(fileButton);
 		topPanel.add(viewButton);
 		topPanel.add(toolsButton);
 		topPanel.add(helpButton);
 		
-		JFrame frame= new JFrame();
+		JFrame frame= new JFrame("MyNotePad");
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		frame.setSize(500,500);
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
+		//Border layout to place sections 
 		frame.setLayout(new BorderLayout());
 		frame.add(topPanel,BorderLayout.NORTH);
+		frame.setVisible(true);
+		frame.add(text);
 		
 		
 		
